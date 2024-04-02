2942d6971727fa7817ed39e9633582cca54c9be9
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-04-02 18:26:07 +0200

    task to implement taking into account shared resource while parallelization

--- src/planning/parallelization/parallelStagesList.ts
+++ src/planning/parallelization/parallelStagesList.ts
@@ -3,21 +3,11 @@ import type { ParallelStages } from '..';
 export class ParallelStagesList {
   constructor(public readonly all: ParallelStages[]) {}
 
-  public static empty(): ParallelStagesList {
-    return new ParallelStagesList([]);
-  }
-
   public static of(...stages: ParallelStages[]): ParallelStagesList {
     return new ParallelStagesList(stages);
   }
 
   public print() {
     return this.all.map((stages) => stages.print()).join(' | ');
-  }
-
-  public add(newParallelStages: ParallelStages): ParallelStagesList {
-    const result = [...this.all, newParallelStages];
-
-    return new ParallelStagesList(result);
   }
 }
--- src/planning/parallelization/stagesToNodes.ts
+++ src/planning/parallelization/stagesToNodes.ts
@@ -14,38 +14,10 @@ export const StagesToNodes = {
     for (let i = 0; i < stages.length; i++) {
       const stage = stages[i];
       result = explicitDependencies(stage, result);
-      result = sharedResources(stage, stages.slice(i + 1), result);
     }
 
     return new Nodes(ObjectSet.from<Node>([...result.values()]));
   },
-};
-
-const sharedResources = (
-  stage: Stage,
-  withStages: Stage[],
-  result: Map<string, Node>,
-): Map<string, Node> => {
-  for (const other of withStages) {
-    if (stage.name !== other.name) {
-      if (
-        stage.resources.some((r) =>
-          other.resources.some((oth) => oth.name === r.name),
-        )
-      ) {
-        if (other.resources.length > stage.resources.length) {
-          let node = result.get(stage.name)!;
-          node = node.dependsOn(result.get(other.name)!);
-          result.set(stage.name, node);
-        } else {
-          let node = result.get(other.name)!;
-          node = node.dependsOn(result.get(stage.name)!);
-          result.set(other.name, node);
-        }
-      }
-    }
-  }
-  return result;
 };
 
 const explicitDependencies = (
