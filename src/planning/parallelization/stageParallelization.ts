8c013d872ddc4cf1a88cf9debe9fde694caa4e85
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-04-02 09:52:46 +0200

    functional workflow if you really like it 🤷

--- src/planning/parallelization/stageParallelization.ts
+++ src/planning/parallelization/stageParallelization.ts
@@ -1,14 +1,20 @@
 import { GraphTopologicalSort, Nodes, SortedNodes } from '../../sorter';
 import type { ObjectSet } from '../../utils/objectSet';
+import { pipe } from '../../utils/pipe';
 import { ParallelStagesList } from './parallelStagesList';
 import { SortedNodesToParallelizedStages } from './sortedNodesToParallelizedStages';
 import { type Stage } from './stage';
 import { StagesToNodes } from './stagesToNodes';
 
+const CREATE_NODES = (stages: Stage[]): Nodes<Stage> =>
+  StagesToNodes.calculate(stages);
+const GRAPH_SORT = (nodes: Nodes<Stage>): SortedNodes<Stage> =>
+  GraphTopologicalSort.sort(nodes);
+const PARALLELIZE = (nodes: SortedNodes<Stage>) =>
+  SortedNodesToParallelizedStages.calculate(nodes);
+
+const WORKFLOW = pipe(CREATE_NODES, GRAPH_SORT, PARALLELIZE);
+
 export const StageParallelization = {
-  of: (stages: ObjectSet<Stage>): ParallelStagesList => {
-    const nodes: Nodes<Stage> = StagesToNodes.calculate(stages);
-    const sortedNodes: SortedNodes<Stage> = GraphTopologicalSort.sort(nodes);
-    return SortedNodesToParallelizedStages.calculate(sortedNodes);
-  },
+  of: (stages: ObjectSet<Stage>): ParallelStagesList => WORKFLOW(stages),
 };
